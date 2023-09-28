173c6358421920ae2c3df934b7d0a729b53557d4
Florent Huck <flovntp@gmail.com>
2023-09-28 16:52:08 +0200

    test

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.get('/', (req, res) => {
 
 MariaDB Tests: ${process.env.RELATIONSHIPS_JSON}
 
-` + JSON.parse(process.env.RELATIONSHIPS_JSON) + ' ' + credentials + ' ' + JSON.stringify(credentials.mariadb) ;
+` + JSON.parse(process.env.RELATIONSHIPS_JSON) + ' ' + credentials + ' ' + JSON.stringify(credentials.mariadb.0) ;
 
     res.set('Content-Type', 'text/plain');
     res.send(outputString);
