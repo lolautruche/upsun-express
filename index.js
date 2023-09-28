d3c71b4218e667ab1041e0c433a8f74aded7174f
Florent Huck <flovntp@gmail.com>
2023-09-28 15:41:07 +0200

    test

--- index.js
+++ index.js
@@ -2,48 +2,48 @@ const app = require('express')();
 const mysql = require("mysql2/promise");
 var port = (process.env.PORT || '3000');
 
-function openConnection() {
-    // var credentials = JSON.stringify(process.env.RELATIONSHIPS_JSON)
-    //
-    // console.log(credentials)
-    //
-    // return mysql.createConnection({
-    //     host: credentials['host'],
-    //     port: credentials['port'],
-    //     user: credentials['username'],
-    //     password: credentials['password'],
-    //     database: credentials['path']
-    // });
-}
-
-function createTable(connection) {
-    return connection.execute(
-        `CREATE TABLE IF NOT EXISTS platforminfo (
-      uid INT(10) NOT NULL AUTO_INCREMENT,
-      username VARCHAR(64) NULL DEFAULT NULL,
-      departname VARCHAR(128) NULL DEFAULT NULL,
-      created DATE NULL DEFAULT NULL,
-      PRIMARY KEY (uid)
-    ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;`
-    );
-}
-
-function insertData(connection) {
-    return connection.execute(
-        "INSERT INTO platforminfo (username, departname, created) VALUES ('platform', 'Deploy Friday', '2019-06-17')"
-    );
-}
-
-function readData(connection) {
-    return connection.query("SELECT * FROM platforminfo");
-}
-
-function dropTable(connection) {
-    return connection.execute("DROP TABLE platforminfo");
-}
+// function openConnection() {
+//     // var credentials = JSON.stringify(process.env.RELATIONSHIPS_JSON)
+//     //
+//     // console.log(credentials)
+//     //
+//     // return mysql.createConnection({
+//     //     host: credentials['host'],
+//     //     port: credentials['port'],
+//     //     user: credentials['username'],
+//     //     password: credentials['password'],
+//     //     database: credentials['path']
+//     // });
+// }
+//
+// function createTable(connection) {
+//     return connection.execute(
+//         `CREATE TABLE IF NOT EXISTS platforminfo (
+//       uid INT(10) NOT NULL AUTO_INCREMENT,
+//       username VARCHAR(64) NULL DEFAULT NULL,
+//       departname VARCHAR(128) NULL DEFAULT NULL,
+//       created DATE NULL DEFAULT NULL,
+//       PRIMARY KEY (uid)
+//     ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;`
+//     );
+// }
+//
+// function insertData(connection) {
+//     return connection.execute(
+//         "INSERT INTO platforminfo (username, departname, created) VALUES ('platform', 'Deploy Friday', '2019-06-17')"
+//     );
+// }
+//
+// function readData(connection) {
+//     return connection.query("SELECT * FROM platforminfo");
+// }
+//
+// function dropTable(connection) {
+//     return connection.execute("DROP TABLE platforminfo");
+// }
 
 // Define the main route.
-app.get('/', async function(req, res){
+app.get('/', (req, res) => {
 
 
 
@@ -83,7 +83,6 @@ app.get('/', async function(req, res){
 MariaDB Tests:
 
 ${process.env.RELATIONSHIPS_JSON}
-
 `;
 
     res.set('Content-Type', 'text/plain');
@@ -92,6 +91,6 @@ ${process.env.RELATIONSHIPS_JSON}
 });
 
 // Get PORT and start the server
-app.listen(port, function() {
+app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
