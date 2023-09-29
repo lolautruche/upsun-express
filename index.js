dbee52eb95ca24cdf16f98f7275b1118f325e44d
Florent Huck <flovntp@gmail.com>
2023-09-29 09:05:01 +0200

    test

--- index.js
+++ index.js
@@ -80,12 +80,13 @@ app.get('/', (req, res) => {
 //
 //     // Make the output.
     var credentials = JSON.parse(process.env.RELATIONSHIPS_JSON)
+    var database =JSON.parse(credentials.mariadb);
     const outputString = `Hello, World! - A simple Express web framework template for Platform.sh
 
 MariaDB Tests: ${process.env.RELATIONSHIPS_JSON}
 
 ` + JSON.parse(process.env.RELATIONSHIPS_JSON) + ' ' + credentials + `
-         host:` + credentials.mariadb['host'];
+         host:` + database['host'];
 
     res.set('Content-Type', 'text/plain');
     res.send(outputString);
