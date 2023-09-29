2f740805c4401754f87e3f4fade27d55c6c752a0
Florent Huck <flovntp@gmail.com>
2023-09-29 10:09:02 +0200

    test

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const mysql = require("mysql");
 var port = (process.env.PORT || '3000');
 
 function openConnection() {
-    let connection = mysql.createConnection({
+    return mysql.createConnection({
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE
     });
-    return connection.connect();
 }
 
 function createTable(connection) {
