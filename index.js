fb6ff56c7dc0a753de3576f1249eded63a5644fe
Florent Huck <flovntp@gmail.com>
2023-09-29 10:18:40 +0200

    test

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get('/', async function(req, res){
 
     const rows = await readData(connection);
 
-    console.log(${rows[0]})
+    console.log(rows[0])
 
     const droppedResult = await dropTable(connection);
 
